05bc9a58ecee320a9252644f13667354f67db09c
Wellton Quirino <welltonq@outlook.com>
2024-05-06 16:43:20 -0300

    fix: add header in api request

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,5 +1,9 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: `${import.meta.env.VITE_REACT_API_URL}`
+  baseURL: `${import.meta.env.VITE_REACT_API_URL}`,
+
+  headers: {
+    'Content-Type': 'application/json'
+  }
 })
