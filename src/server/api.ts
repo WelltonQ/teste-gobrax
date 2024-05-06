6a54f84cc41209dbe73a37b2eedb9ab82ab9c6ce
Wellton Quirino <welltonq@outlook.com>
2024-05-06 17:29:30 -0300

    fix: add accept json in axios

--- src/server/api.ts
+++ src/server/api.ts
@@ -4,6 +4,7 @@ export const api = axios.create({
   baseURL: `${import.meta.env.VITE_REACT_API_URL}`,
 
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
   }
 })
